e927ec13826e770d3725e23fdefa259ae5ad6bdc
Leonardo Conegundes <leonardo@s10i.com.br>
2013-07-10 00:42:55 -0300

    fixed bug in web.js to read content from a file

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var express = require('express');
 
-var text = fs.readFileSync('index.html');
+var buf = fs.readFileSync('index.html');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
