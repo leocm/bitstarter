270309bbad3ec48a183cd51d2c32078f8b2ab718
Leonardo Conegundes <leonardo@s10i.com.br>
2013-07-10 00:26:47 -0300

    updated web.js to galo 4 x 1 newells

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('GALO 4 X 1 NEWELLS');
 });
 
 var port = process.env.PORT || 5000;
